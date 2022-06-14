0491c4c6650c3604901c3210cc5a9fe9f85fbbfc
Neel <98832330+Neelk9@users.noreply.github.com>
2022-06-14 13:46:27 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+document.querySelector('.header__moon').onclick = function() {
+    document.querySelector('body').classList.toggle('is-black');
+}
+
+document.querySelector('.header__mob-menu').onclick = function() {
+    document.querySelector('.header-menu').classList.toggle('is-active');
+}
+
+document.querySelector('.bx bxl-linkedin').onclick = function() {
+    window.location='https://google.com';
+}
